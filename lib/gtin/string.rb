b6b1802b8db48c325f2bad4481df756040e1e9ac
G. Allen Morris III <gam3@gam3.net>
2014-03-08 06:17:50 -0800

    remove size from string methods

--- lib/gtin/string.rb
+++ lib/gtin/string.rb
@@ -3,17 +3,15 @@ require 'gtin'
 
 module Gtin14
   # return a GTIN object from a string
-  # @attr [Fixnum] maximum size of the gtin
   # @raises ArgumentError "Not a valid GTIN"
-  def is_gtin?(type = nil)
-    Gtin.is_gtin?(self, type)
+  def is_gtin?()
+    Gtin.is_gtin?(self)
   end
 
   # return a GTIN object from a string
-  # @attr [Fixnum] maximum size of the gtin
   # @raises ArgumentError "Not a valid GTIN"
-  def to_gtin(size = nil)
-    Gtin.new(self, size)
+  def to_gtin()
+    Gtin.new(self)
   end
 end
 
